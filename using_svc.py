46465f0bd11473fa0858308b3a576a78123beb33
이창민 <lcentimeter@hanyang.ac.kr>
2023-06-09 13:56:28 +0900

    Update using_svc.py

--- using_svc.py
+++ using_svc.py
@@ -7,24 +7,20 @@ from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 import os
 
-# Load the dataset
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
-# Define a directory to store the extracted features
 features_dir = 'extracted_features'
 os.makedirs(features_dir, exist_ok=True)
 
-# Preprocess the audio data and extract acoustic features
 def preprocess_audio(file_path):
-    # Check if the features are already extracted and saved
     features_file = os.path.join(features_dir, os.path.splitext(os.path.basename(file_path))[0] + '.npy')
     if os.path.exists(features_file):
         print(f"Loading features from {features_file}")
         return np.load(features_file)
     
     audio, sample_rate = librosa.load(file_path, res_type='kaiser_fast', mono=True)
-    # Extract pitch/frequency, intensity/loudness, tempo/rhythm, and timbre/tone features
+    
     pitch = librosa.yin(audio, fmin=100, fmax=1000)
     energy = np.mean(audio ** 2)
     onset_env = librosa.onset.onset_strength(y=audio, sr=sample_rate)
@@ -33,7 +29,6 @@ def preprocess_audio(file_path):
     delta_mfccs = librosa.feature.delta(mfccs)
     delta2_mfccs = librosa.feature.delta(mfccs, order=2)
     
-    # Pad or truncate the features to a fixed size
     max_len = 1000
     pitch = np.pad(pitch, (0, max_len - len(pitch)))
     mfccs = np.pad(mfccs, ((0, 0), (0, max_len - mfccs.shape[1])))
@@ -41,7 +36,6 @@ def preprocess_audio(file_path):
     delta2_mfccs = np.pad(delta2_mfccs, ((0, 0), (0, max_len - delta2_mfccs.shape[1])))
     features = np.concatenate((pitch, [energy], [tempo], mfccs.flatten(), delta_mfccs.flatten(), delta2_mfccs.flatten()))
     
-    # Save the extracted features
     np.save(features_file, features)
     
     print(f"Extracted features saved to {features_file}")
@@ -50,42 +44,35 @@ def preprocess_audio(file_path):
 train_df['audio_features'] = train_df['path'].apply(preprocess_audio)
 test_df['audio_features'] = test_df['path'].apply(preprocess_audio)
 
-# Prepare the data for machine learning
 X = np.stack(train_df['audio_features'].values)
 y = train_df['label'].values
 
-# Split the data
 X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Encode the labels
 label_encoder = LabelEncoder()
 y_train_encoded = label_encoder.fit_transform(y_train)
 y_val_encoded = label_encoder.transform(y_val)
 
 print("Training the model...")
-# Design and train a model
 model = SVC()
 model.fit(X_train, y_train_encoded)
 
-# Evaluate the model
 val_predictions = model.predict(X_val)
 val_accuracy = accuracy_score(y_val_encoded, val_predictions)
 print("Validation accuracy:", val_accuracy)
 
-# Predict emotions for the test set
+
 X_test = np.stack(test_df['audio_features'].values)
 test_predictions = model.predict(X_test)
 test_df['predicted_label'] = label_encoder.inverse_transform(test_predictions)
 
-# Save predictions or further analysis
+
 test_df.to_csv('test_predictions.csv', index=False)
 
 
-# Create a new DataFrame with 'id' and 'label' columns
+
 submission_df = pd.DataFrame({
     'id': test_df['id'],
     'label': test_df['predicted_label']
 })
-
-# Save the output DataFrame to a new CSV file
 submission_df.to_csv('submission.csv', index=False)
